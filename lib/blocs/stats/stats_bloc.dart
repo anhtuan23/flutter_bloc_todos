abd34dc2fc047e58bee6c3c1d60b801aab3020ab
Do Tuan Anh <dotuananh.dta@gmail.com>
2020-07-03 08:34:04 +0700

    implement StatsBloc

--- lib/blocs/stats/stats_bloc.dart
+++ lib/blocs/stats/stats_bloc.dart
@@ -2,18 +2,42 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc_todo/blocs/todos/todos_bloc.dart';
+import 'package:flutter_bloc_todo/models/models.dart';
+import 'package:meta/meta.dart';
 
 part 'stats_event.dart';
 part 'stats_state.dart';
 
 class StatsBloc extends Bloc<StatsEvent, StatsState> {
-  @override
-  StatsState get initialState => StatsInitial();
+  final TodosBloc todosBloc;
+  StreamSubscription todosSubscription;
+
+  StatsBloc({@required this.todosBloc}) {
+    todosSubscription = todosBloc.listen((state) {
+      if (state is TodosLoadSuccess) {
+        add(StatsUpdated(state.todos));
+      }
+    });
+  }
 
   @override
-  Stream<StatsState> mapEventToState(
-    StatsEvent event,
-  ) async* {
-    // TODO: implement mapEventToState
+  StatsState get initialState => StatsLoadInProgress();
+
+  @override
+  Stream<StatsState> mapEventToState(StatsEvent event) async* {
+    if (event is StatsUpdated) {
+      int numActive =
+          event.todos.where((todo) => !todo.complete).toList().length;
+      int numCompleted =
+          event.todos.where((todo) => todo.complete).toList().length;
+      yield StatsLoadSuccess(numActive, numCompleted);
+    }
+  }
+
+  @override
+  Future<void> close() {
+    todosSubscription.cancel();
+    return super.close();
   }
 }
