3238036cec73fffa5c077bebe346314b910c93d4
Do Tuan Anh <dotuananh.dta@gmail.com>
2020-07-03 08:54:05 +0700

    implement TabBloc

---
+++ lib/blocs/tab/tab_bloc.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc_todo/models/models.dart';
+
+part 'tab_event.dart';
+part 'tab_state.dart';
+
+class TabBloc extends Bloc<TabEvent, AppTab> {
+  @override
+  AppTab get initialState => AppTab.todos;
+
+  @override
+  Stream<AppTab> mapEventToState(TabEvent event) async* {
+    if (event is TabUpdated) {
+      yield event.tab;
+    }
+  }
+}
---
+++ lib/blocs/tab/tab_state.dart
@@ -0,0 +1,10 @@
+part of 'tab_bloc.dart';
+
+abstract class TabState extends Equatable {
+  const TabState();
+}
+
+class TabInitial extends TabState {
+  @override
+  List<Object> get props => [];
+}
